d7a734083fb74c9ff6a9be240064e650f67473b8
arikacaribu <arikacaribu@gmail.com>
2020-06-30 10:53:52 +0700

    test

--- application/views/admin/operator.php
+++ application/views/admin/operator.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title> DASHBOARD OPERATOR</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/admin/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
