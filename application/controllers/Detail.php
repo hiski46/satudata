c3df6ced592f8ada07face7197b7cb9d00914d6a
hiski46 <hiskia.14117091@student.itera.ac.id>
2020-06-30 17:30:08 +0700

    integrasi menu detail ke database

--- application/controllers/Detail.php
+++ application/controllers/Detail.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Detail extends CI_Controller {
 
 	public function index(){
-        $this->load->view('dashboard/detail');
+        // $this->load->view('dashboard/detail');
     }
 }
