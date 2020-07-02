30719319c5661868797c81d9cc8b681e6b9484f5
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2020-07-02 10:36:36 +0700

    menu admin

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -2,12 +2,12 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-        $this->load->view('admin/admin');
+        $this->load->view('admin/admin', $data);
     }
 }
