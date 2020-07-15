b2081676db3871d2f15d66fd54ae15122bbd303a
hiski46 <hiskia.14117091@student.itera.ac.id>
2020-07-15 15:03:33 +0700

    fitur ubah password dari super admin

--- application/controllers/admin/DaftarOPD.php
+++ application/controllers/admin/DaftarOPD.php
@@ -8,6 +8,7 @@ class DaftarOPD extends CI_Controller
     {
         parent::__construct();
         $this->load->model('DataModel');
+        $this->load->library('form_validation');
     }
     public function index()
     {
@@ -23,5 +24,35 @@ class DaftarOPD extends CI_Controller
         $id_user=$this->uri->segment(4);
         $this->DataModel->hapus_user($id_user);
         redirect('admin/daftaropd');
+    }
+
+    public function gantipassword()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
+        $this->form_validation->set_rules('pass1', 'Password Lama', 'required|trim' );
+        $this->form_validation->set_rules('pwbaru', 'Password Baru', 'required|trim|matches[ulangpw]|min_length[6]' );
+        $this->form_validation->set_rules('ulangpw','Ulang Password', 'required|trim|matches[pwbaru]|min_length[6]' );
+
+        
+        if ($this->form_validation->run()== false) {
+            // $this->load->view('admin/DaftarOPD',$data);
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Belum Diubah</div>');
+                redirect('admin/daftaropd');
+        }else{
+            $password_lama = $this->input->post('pass1');
+            $password_baru = $this->input->post('pwbaru');
+            if(!password_verify($password_lama, $data['user']['password'] )){
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Pasword Anda Salah</div>');
+                redirect('admin/daftaropd');
+            }else{
+                $password_hash = password_hash($password_baru, PASSWORD_DEFAULT);
+                $this->db->set('password', $password_hash);
+                $this->db->where('id', $this->input->post('id'));
+                $this->db->update('public.user');
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password Diubah</div>');
+                redirect('admin/daftaropd');
+            }
+        }
     }
 }
