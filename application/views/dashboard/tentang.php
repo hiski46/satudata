a6b8ffb1ddb3bee3029866821a840eb2eb703aa5
arikacaribu <arikacaribu@gmail.com>
2020-07-20 12:06:23 +0700

    membuat halaman about

---
+++ application/views/dashboard/tentang.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Tentang</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="http://localhost/satudata/assets/assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="http://localhost/satudata/assets/css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url() ?>">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url("data") ?>">Data</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url("galery") ?>">Galeri</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url("tentang") ?>">Tentang</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url("admin/login") ?>">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Services-->
+    <section class="page-section" id="services">
+       
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Kabupaten Karo</h2>
+                <h3 class="section-subheading text-muted">Dalam Satu Data</h3>
+            </div>
+                          <div class="card border-primary mb-3 mx-auto" style="max-width: 75%;">
+                            <div class="card-header">
+                               <H3> TENTANG </H3> 
+                            </div>
+                            <div class="card-body">
+                                <blockquote class="blockquote mb-0">
+                                <p>Portal Satu Data Indonesia (data.go.id) merupakan portal resmi data terbuka Indonesia yang berisi data lintas kementerian, lembaga pemerintahan, pemerintahan daerah, 
+                                    dan semua instansi lain yang terkait yang menghasilkan data terkait Indonesia. 
+                                    Satu Data Indonesia adalah sebuah inisiatif pemerintah untuk memperbaiki dan meningkatkan kualitas
+                                     tata kelola data pemerintah. Pemanfaatan data pemerintah tidak sebatas untuk pengambilan kebijakan, 
+                                     tetapi juga sebagai bentuk pemenuhan kebutuhan data publik bagi masyarakat.
+                                     <br>
+                                     <br>
+
+                                    Melalui Satu Data, Kantor Staf Presiden mendukung dan berupaya penuh untuk melakukan
+                                    perbaikan terhadap data pemerintah Indonesia. Data tersedia dalam format terbuka dan mudah digunakan kembali, dengan tujuan untuk meningkatkan transparansi dan akuntabilitas pemerintah, serta untuk meningkatkan partisipasi masyarakat dalam mengawal pembangunan. Kami menyediakan data dalam format yang mudah dicari, diakses serta digunakan dengan harapan bahwa pengguna portal ini dapat memanfaatkan data yang tersedia untuk mewujudkan dan mendukung pembangunan Indonesia. Seluruh kumpulan data dalam portal ini dikategorikan sebagai data publik, sehingga tidak diperkenankan memuat informasi yang mengandung rahasia negara, rahasia pribadi atau hal lain sejenisnya dan sudah diatur dalam ketetapan Undang-Undang Nomor 14 Tahun 2008 tentang Keterbukaan Informasi Publik.</p>
+                                    
+                                </blockquote>
+                            </div>
+                            </div>
+                        
+                    </div>
+
+              
+                <!-- <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-4">
+                        <div class="item">
+                            <a href="http://localhost/satudata/detail">
+                                <span class="notify-badge">0</span>
+                                <img src="http://localhost/satudata/assets/assets/img/logokaro.gif" alt="" class="img-responsive img-fluid">
+                                <h4 class="my-3">DISKOMINFO</h4>
+                            </a>
+                            <p class="text-muted">Dinas Komunikasi dan Informatika</p>
+                        </div>
+                    </div> -->
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Footer-->
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+                <div class="col-lg-4 my-3 my-lg-0">
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="#"><i class="fab fa-instagram"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col-lg-4 text-lg-right"><a class="mr-3" href="#!">Privacy Policy</a><a href="#!">Terms of Use</a></div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <script src="http://localhost/satudata/assets//css/styles.css/assets/mail/jqBootstrapValidation.js"></script>
+    <script src="http://localhost/satudata/assets//css/styles.css/assets/mail/contact_me.js"></script>
+    <!-- Core theme JS-->
+    <script src="http://localhost/satudata/assets//css/styles.css/js/scripts.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
