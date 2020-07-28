e8c48bb1c84b2a3d165860982b4c6c56a0996c06
arikacaribu <arikacaribu@gmail.com>
2020-07-28 16:38:58 +0700

    edit upload galeri

--- application/views/admin/uploadgaleri.php
+++ application/views/admin/uploadgaleri.php
@@ -254,8 +254,7 @@
 
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                    <input type="submit" value="upload" class="btn btn-primary">
+                    
                 </div>
             </div>
         </div>
