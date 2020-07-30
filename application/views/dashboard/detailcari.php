26ac33fe7f635ff58ffb7f175493608ffe59bec0
arikacaribu <arikacaribu@gmail.com>
2020-07-30 14:17:21 +0700

    edit dashboard

--- application/views/dashboard/detailcari.php
+++ application/views/dashboard/detailcari.php
@@ -99,7 +99,7 @@
             <footer class="footer py-4">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+                        <div class="col-lg-4 text-lg-left">Copyright © The Most Angry 2020</div>
                         <div class="col-lg-4 my-3 my-lg-0">
                             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
                         </div>
--- application/views/dashboard/galery.php
+++ application/views/dashboard/galery.php
@@ -83,7 +83,7 @@
     <footer class="footer py-4">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+                <div class="col-lg-4 text-lg-left">Copyright © The Most Angry 2020</div>
                 <div class="col-lg-4 my-3 my-lg-0">
                 <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/dinkominfokaro"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/kominfo.karo/"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/dinkominfokaro/"><i class="fab fa-instagram"></i></a>
                 </div>
